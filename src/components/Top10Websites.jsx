25fc7290b4126b05397550411cf5caf014023b72
Mariya Villamor <mariyavillamor@gmail.com>
2023-09-04 22:16:33 +0800

    :sparkles: feat: update website content dynamically based on selected index

--- src/components/Top10Websites.jsx
+++ src/components/Top10Websites.jsx
@@ -3,15 +3,16 @@ import '../styles/Top10Websites.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
-const Top10Websites = () => {
+const Top10Websites = ({ handleWebsiteChange }) => {
   const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 4; // Number of items per page
+  const itemsPerPage = 4;
+
   const top10Items = [
+    '/mariyaposter.png',
     '/nexus.png',
     '/techblazers.png',
     '/exploredia.png',
     '/cuisina.png',
-    '/mariyaposter.png',
     // Add URLs for other websites
   ];
 
@@ -23,21 +24,15 @@ const Top10Websites = () => {
 
   return (
     <div className="top-10-items">
-      <h2>Continue Watching</h2> {/* Add the title */}
+      <h2>Continue Watching</h2>
       <div className="item-list">
-        <button
-          className={`arrow-button left-arrow ${currentPage === 1 ? 'hidden' : ''}`}
-          onClick={() => paginate(currentPage - 1)}
-        >
-          <FontAwesomeIcon icon={faAngleLeft} /> {/* Left arrow */}
-        </button>
         {currentItems.map((item, index) => (
-          <div key={index} className="item-item">
+          <div key={index} className="item-item" onClick={() => handleWebsiteChange(indexOfFirstItem + index)}>
             <img
               src="./favicon.png"
               alt="mariya-logo"
               className="logo"
-            /> {/* Logo */}
+            />
             <span className="websites-number">{index + 1 + (currentPage - 1) * itemsPerPage}</span>
             <img
               src={item}
@@ -46,14 +41,21 @@ const Top10Websites = () => {
             />
           </div>
         ))}
+      </div>
+      <div className="pagination">
+        <button
+          className={`arrow-button left-arrow ${currentPage === 1 ? 'hidden' : ''}`}
+          onClick={() => paginate(currentPage - 1)}
+        >
+          <FontAwesomeIcon icon={faAngleLeft} />
+        </button>
         <button
           className="arrow-button right-arrow"
           onClick={() => paginate(currentPage + 1)}
         >
-          <FontAwesomeIcon icon={faAngleRight} /> {/* Right arrow */}
+          <FontAwesomeIcon icon={faAngleRight} />
         </button>
       </div>
-      <div className="pagination"></div>
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/Home.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlay, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+import { faPlay, faInfoCircle, faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 import Top10Websites from '../components/Top10Websites';
 
 const Home = () => {
+  const websiteConfigs = [
+    {
+      seriesTitle: 'Hello, I\'m',
+      websiteTitle: 'MARIYA',
+      summary: 'a passionate web developer with a love for creating amazing web experiences. I enjoy working on both front-end and back-end technologies and am always excited to take on new challenges.',
+      backgroundImageUrl: '/mariyaposter1.png'
+    },
+    {
+      seriesTitle: 'We link Worlds',
+      websiteTitle: 'NEXUS',
+      summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur aliquet est, eget egestas metus congue vel. Donec sed urna sed tellus venenatis hendrerit. Nulla facilisi. Sed tristique tortor nec scelerisque.',
+      backgroundImageUrl: '/nexus1.png'
+    },
+    {
+      seriesTitle: 'Introducing Canvas 2.0',
+      websiteTitle: 'TECHBLAZERS',
+      summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sit amet aliquam arcu. Nulla facilisi. Proin fringilla quam ac odio auctor facilisis. Vivamus auctor neque eu lectus egestas, at vulputate velit euismod.',
+      backgroundImageUrl: '/techblazers1.png'
+    },
+    {
+      seriesTitle: 'Your Ultimate Country Guide',
+      websiteTitle: 'EXPLOREDIA',
+      summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sit amet aliquam arcu. Nulla facilisi. Proin fringilla quam ac odio auctor facilisis. Vivamus auctor neque eu lectus egestas, at vulputate velit euismod.',
+      backgroundImageUrl: '/exploredia1.png'
+    },
+    {
+      seriesTitle: 'Go On. Get your recipe out.',
+      websiteTitle: 'CUISINA',
+      summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sit amet aliquam arcu. Nulla facilisi. Proin fringilla quam ac odio auctor facilisis. Vivamus auctor neque eu lectus egestas, at vulputate velit euismod.',
+      backgroundImageUrl: '/cuisina1.png',
+    },
+    // Add more configurations for other websites
+  ];
+
+  const [currentWebsite, setCurrentWebsite] = useState(websiteConfigs[0]);
+
+  const handleWebsiteChange = (configIndex) => {
+    setCurrentWebsite(websiteConfigs[configIndex]);
+  };
+
   return (
     <div className="home">
-      <div className="background-poster">
-
-      </div>
+      <div className="background-poster" style={{ backgroundImage: `url(${currentWebsite.backgroundImageUrl})` }}></div>
       <div className="home-body">
         <div className="header-logo">
           <img
@@ -18,19 +56,13 @@ const Home = () => {
           />
         </div>
         <div className="series">
-          <p>Hello, I'm</p>
+          <p>{currentWebsite.seriesTitle}</p>
         </div>
         <div className="website-title">
-          {/* website title */}
-          <h1>MARIYA</h1>
+          <h1>{currentWebsite.websiteTitle}</h1>
         </div>
         <div className="summary">
-          {/* website summary */}
-          <p>
-            a passionate web developer with a love for creating
-            amazing web experiences. I enjoy working on both front-end and back-end
-            technologies and am always excited to take on new challenges.
-          </p>
+          <p>{currentWebsite.summary}</p>
         </div>
         <div className="buttons">
           <button className="play-button">
@@ -40,7 +72,7 @@ const Home = () => {
             <FontAwesomeIcon icon={faInfoCircle} /> More Info
           </button>
         </div>
-        <Top10Websites />
+        <Top10Websites handleWebsiteChange={handleWebsiteChange} />
       </div>
     </div>
   );
